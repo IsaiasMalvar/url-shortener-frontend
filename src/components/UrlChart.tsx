5e5af449ead236c8ec286832f800abcfa1f28d14
Isaias Malvar <isaiasmg@93hotmail.com>
2025-01-31 18:33:16 +0000

    Add total click amount info to chart

--- src/components/UrlChart.tsx
+++ src/components/UrlChart.tsx
@@ -10,7 +10,12 @@ import {
 import { Bar } from "react-chartjs-2";
 import { useEffect, useState } from "react";
 import { useFetchClicksPerUrl, useFetchTotalClicks } from "../hooks/useQuery";
-import { chartOptions, getDataset, getLabels } from "../utils/chartConfig";
+import {
+  chartOptions,
+  getDataset,
+  getLabels,
+  getTotalClicksAmount,
+} from "../utils/chartConfig";
 import toast from "react-hot-toast";
 
 interface URLChart {
@@ -33,12 +38,15 @@ const UrlChart = ({ slug, token }: URLChart) => {
 
   const [labels, setLabels] = useState<string[]>([]);
   const [dataset, setDataset] = useState<number[]>([]);
+  const [totalClicks, setTotalClicks] = useState<number>(0);
 
   const dataSet = {
     labels,
     datasets: [
       {
-        label: slug ? `${slug} Analytics` : "Total Clicks",
+        label: slug
+          ? `${slug} Clicks: ${totalClicks}`
+          : "Total Clicks: " + totalClicks,
         data: dataset,
         backgroundColor: "white",
         color: "white",
@@ -51,9 +59,11 @@ const UrlChart = ({ slug, token }: URLChart) => {
     if (slug && urlData) {
       setLabels(getLabels(urlData));
       setDataset(getDataset(urlData));
+      setTotalClicks(getTotalClicksAmount(urlData));
     } else if (totalData) {
       setLabels(getLabels(totalData));
       setDataset(getDataset(totalData));
+      setTotalClicks(getTotalClicksAmount(totalData));
     }
     if (isError) {
       toast.error("Data could not be properly loaded.");
